1b0a59d94af8a57fc2a0b0f12946d854abc9f45b
Mattia <37929258+ch3p4ll3@users.noreply.github.com>
2019-08-24 11:52:39 +0200

    Modified the commands
    Modified the commands to make them accepted by botfather

--- telegram/allMessage.py
+++ telegram/allMessage.py
@@ -19,10 +19,10 @@ commands = {
                 'help_BeforeAuthorized':
                     u'Command:\n'
                     u'/off - disconnect from bot\n'
-                    u'/setsshUser - Set ssh user and ssh password\n'
-                    u'/setsshHost - Set host(IP) for ssh connection\n'
+                    u'/setsshuser - Set ssh user and ssh password\n'
+                    u'/setsshhost - Set host(IP) for ssh connection\n'
                     u'/information - Show information, about ssh-connection\n'
-                    u'/aboutBot - Information about bot and author\n',
+                    u'/aboutbot - Information about bot and author\n',
 
                 'aboutBot': 'Author GitHub - https://github.com/vzemtsov\n'
                     u'Please write wishes to improve this bot',
